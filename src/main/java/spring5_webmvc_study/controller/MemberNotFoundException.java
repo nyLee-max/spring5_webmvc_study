cc7317928a528494a7db9c4d9405fe5dfe049dd6
nyLee <skdus2421@naver.com>
2021-05-20 10:08:52 +0900

    초기화

---
+++ src/main/java/spring5_webmvc_study/controller/MemberNotFoundException.java
@@ -0,0 +1,14 @@
+package spring5_webmvc_study.controller;
+
+public class MemberNotFoundException extends RuntimeException {
+
+	public MemberNotFoundException() {
+		super();
+	}
+
+	public MemberNotFoundException(String arg0) {
+		super(arg0);
+	}
+
+	
+}
