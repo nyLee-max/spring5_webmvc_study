4f1d73dd2192838ef02186639a41ca843afa45ff
nyLee <skdus2421@naver.com>
2021-05-27 16:03:53 +0900

    ㅇㅇ

---
+++ src/main/java/spring5_webmvc_study/controller/ErrorResponse.java
@@ -0,0 +1,14 @@
+package spring5_webmvc_study.controller;
+
+public class ErrorResponse {
+	private String message;
+
+	public ErrorResponse() {
+	}
+
+	public ErrorResponse(String message) {
+		super();
+		this.message = message;
+	}
+
+}
--- src/main/java/spring5_webmvc_study/controller/RestMemberController.java
+++ src/main/java/spring5_webmvc_study/controller/RestMemberController.java
@@ -1,11 +1,17 @@
 package spring5_webmvc_study.controller;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,26 +31,44 @@ public class RestMemberController {
 	public List<Member> members() {
 		return memberDao.selectAll();
 	}
+	
+	@ExceptionHandler(MemberNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleNoData(){
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("no member"));
+	}
+	
+	@PostMapping("/api/members")
+	public ResponseEntity<Object> newMember(@RequestBody RegisterRequest regReq, Errors errors,
+			HttpServletResponse response) throws IOException {
+		try {
+			new RegisterRequestValidator().validate(regReq, errors);
+			if (errors.hasErrors()) {
+//				response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+//				return ResponseEntity.badRequest().build();
+				String errorCode = errors.getAllErrors()
+									.stream()
+									.map(error->error.getCodes()[0])
+									.collect(Collectors.joining(","));
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+						.body(new ErrorResponse("errorcodes = " + errorCode));
+			}
+			Long newMemberId = registerService.regist(regReq);
+//			response.setHeader("Location", "/api/members/" + newMemberId);
+//			response.setStatus(HttpServletResponse.SC_CREATED);
+			URI uri = URI.create("/api/members/" + newMemberId);
+			return ResponseEntity.created(uri).build();
+		} catch (DuplicateMemberException e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
+
+	}
 
 	@GetMapping("/api/members/{id}")
-	public Member member(@PathVariable Long id, HttpServletResponse response) throws IOException {
+	public ResponseEntity<Object> member(@PathVariable Long id, HttpServletResponse response) throws IOException {
 		Member member = memberDao.selectById(id);
 		if (member == null) {
-			response.sendError(HttpServletResponse.SC_NOT_FOUND);
-			return null;
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("no member"));
 		}
-		return member;
+		return ResponseEntity.status(HttpStatus.OK).body(member);
 	}
-
-	@PostMapping("/api/members")
-	public void newMember(@RequestBody RegisterRequest regReq, HttpServletResponse response) throws IOException {
-		try {
-			Long newMemberId = registerService.regist(regReq);
-			response.setHeader("Location", "/api/members/" + newMemberId);
-			response.setStatus(HttpServletResponse.SC_CREATED);
-		} catch (DuplicateMemberException e) {
-			response.sendError(HttpServletResponse.SC_CONFLICT);
-		}
-	}
-
 }
